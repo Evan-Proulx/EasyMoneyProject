a6a76cc8b9b8a856bba7f76f4d54fd56d1df2607
Evan <evanproulx7@gmail.com>
2023-11-19 19:22:33 -0500

    -Finished loan calculator ui

--- app/src/main/java/com/example/easymoney/Fragments/LoanCalculatorFragment.java
+++ app/src/main/java/com/example/easymoney/Fragments/LoanCalculatorFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.easymoney.R;
 
@@ -61,6 +62,7 @@ public class LoanCalculatorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loan_calculator, container, false);
+        View view = inflater.inflate(R.layout.fragment_loan_calculator, container, false);
+        return view;
     }
 }
