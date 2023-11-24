df28aa0387cf2d5fe1d9e1062a5de603232b098c
Evan <evanproulx7@gmail.com>
2023-11-24 15:40:50 -0500

    -Added code to send data from the loan fragment to the result fragment

--- app/src/main/java/com/example/easymoney/Fragments/LoanResultFragment.java
+++ app/src/main/java/com/example/easymoney/Fragments/LoanResultFragment.java
@@ -3,15 +3,20 @@ package com.example.easymoney.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.easymoney.R;
+import com.example.easymoney.SharedViewModel;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +33,8 @@ public class LoanResultFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private SharedViewModel sharedViewModel;
+
 
     public LoanResultFragment() {
         // Required empty public constructor
@@ -58,6 +65,7 @@ public class LoanResultFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
     }
 
     @Override
@@ -66,9 +74,42 @@ public class LoanResultFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_loan_result, container, false);
 
-        TextView loanResult = view.findViewById(R.id.loanResultTextView);
+        TextView paymentTitleTextView = view.findViewById(R.id.paymentTitleTextView);
+        TextView paymentTextView = view.findViewById(R.id.paymentTextView);
+        TextView totalPaymentTitleTextView = view.findViewById(R.id.totalPaymentTitleTextView);
+        TextView totalPaymentTextView = view.findViewById(R.id.totalPaymentTextView);
+        TextView totalInterestTitleTextView = view.findViewById(R.id.totalInterestTitleTextView);
+        TextView totalInterestTextView = view.findViewById(R.id.totalInterestTextView);
+
+
+        ArrayList<Double> receivedData = receiveDataFromFragment();
+        double calculatedInterestRate = receivedData.get(0);
+        double calculatedPayment = receivedData.get(1);
+        double totalPayment = receivedData.get(2);
+        double totalInterest = receivedData.get(3);
+        double paymentTime = receivedData.get(4);
+
+        //checks what payment time was selected and changes text
+        switch ((int) paymentTime){
+            case 1: paymentTitleTextView.setText("Monthly Payments");break;
+            case 2: paymentTitleTextView.setText("Weekly  Payments");break;
+            case 3: paymentTitleTextView.setText("Yearly Payments");break;
+        }
+
+        paymentTextView.setText(getString(R.string.dollar_format, String.valueOf(calculatedPayment)));
+        totalPaymentTextView.setText(getString(R.string.dollar_format, String.valueOf(totalPayment)));
+        totalInterestTextView.setText(getString(R.string.dollar_format, String.valueOf(totalInterest)));
+
+
+        LinearLayout resultLinearLayout = view.findViewById(R.id.resultLinearLayout);
         Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.loan_animation);
-        loanResult.startAnimation(animation);
+        resultLinearLayout.startAnimation(animation);
         return view;
     }
+
+    //gets data sent from the LoanCalculatorFragment
+    private ArrayList<Double> receiveDataFromFragment(){
+        return sharedViewModel.getSharedData();
+    }
+
 }
